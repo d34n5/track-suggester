afde02f2ad610f49264dcf863e7ffe2fbcb838d0
Dean Scelza <deanscelza@gmail.comgit config --global user.email deanscelza@gmail.com>
2016-08-12 11:06:50 -0700

    fix indentations and spacing

--- track-suggester/js/scripts.js
+++ track-suggester/js/scripts.js
@@ -1,34 +1,27 @@
-$(document).ready(function(){
-  $("#epicodus").submit(function(event){
+$(document).ready(function() {
+  $("#epicodus").submit(function(event) {
     var firstNameInput = $("input#firstName").val();
     var environmentInput = $("input:radio[name=environment]:checked").val();
     var companyInput = $("input:radio[name=company]:checked").val();
     var contentInput = $("#content").val();
     var usersInput = $("#users").val();
     var foodInput = $("#food").val();
-
     $(".firstName").text(firstNameInput);
-
-    if (environmentInput === "front" ){
+    if (environmentInput === "front") {
       $("#cssMessage").show();
     }
-
-    if (companyInput === "large"){
+    if (companyInput === "large") {
       $("#cSharpMessage").show();
-    } else if (companyInput === "small"){
+    } else if (companyInput === "small") {
       $("#javaMessage").show();
     }
-
-    if (foodInput === "Pizza"){
+    if (foodInput === "Pizza") {
       alert("We suggest taking the PHP/Drupal track at Epicodus.")
-    } else if (foodInput === "BBQ"){
+    } else if (foodInput === "BBQ") {
       $("#cSharpMessage").show();
     } else {
       $("#javaMessage").show();
     }
-
-
-
     event.preventDefault();
   });
 });
