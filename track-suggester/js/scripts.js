4909230b2813f23767765f6dea32350c651e1316
Dean Scelza <deanscelza@gmail.comgit config --global user.email deanscelza@gmail.com>
2016-08-12 14:42:13 -0700

    give opacity to the form-groups so as to retain quality of background image

--- track-suggester/js/scripts.js
+++ track-suggester/js/scripts.js
@@ -22,7 +22,7 @@ $(document).ready(function() {
       foodInput === "BBQ") {
       $("#cSharpMessage").slideDown("slow");
     } else {
-      $("#javaMessage").fadeToggle("slow");
+      $("#javaMessage").fadeIn("slow");
     }
     event.preventDefault();
   });
